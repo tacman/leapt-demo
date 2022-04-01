2db76db063ee18a97f3265a4849e4b9d717fe6ea
jmsche <contact@jmsche.fr>
2022-04-01 15:36:39 +0200

    Paginator Bootstrap themes

--- src/Controller/PaginatorController.php
+++ src/Controller/PaginatorController.php
@@ -13,20 +13,50 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/paginator', name: 'app_paginator_')]
 final class PaginatorController extends AbstractController
 {
+    private const ITEMS = [
+        'C', 'C++', 'C#', 'Go', 'Java',
+        'JavaScript', 'Kotlin', 'PHP', 'Python', 'R',
+        'Ruby', 'Rust', 'Swift', 'TypeScript',
+    ];
+
     #[Route('', name: 'index')]
     public function index(Request $request): Response
     {
-        $items = [
-            'C', 'C++', 'C#', 'Go', 'Java',
-            'JavaScript', 'Kotlin', 'PHP', 'Python', 'R',
-            'Ruby', 'Rust', 'Swift', 'TypeScript',
-        ];
-        $paginator = new ArrayPaginator($items);
+        return $this->render('paginator/index.html.twig', [
+            'paginator' => $this->getPaginator($request),
+        ]);
+    }
+
+    #[Route('/bootstrap-3', name: 'bootstrap_3')]
+    public function bootstrap3(Request $request): Response
+    {
+        return $this->render('paginator/bootstrap_3.html.twig', [
+            'paginator' => $this->getPaginator($request),
+        ]);
+    }
+
+    #[Route('/bootstrap-4', name: 'bootstrap_4')]
+    public function bootstrap4(Request $request): Response
+    {
+        return $this->render('paginator/bootstrap_4.html.twig', [
+            'paginator' => $this->getPaginator($request),
+        ]);
+    }
+
+    #[Route('/bootstrap-5', name: 'bootstrap_5')]
+    public function bootstrap5(Request $request): Response
+    {
+        return $this->render('paginator/bootstrap_5.html.twig', [
+            'paginator' => $this->getPaginator($request),
+        ]);
+    }
+
+    private function getPaginator(Request $request): ArrayPaginator
+    {
+        $paginator = new ArrayPaginator(self::ITEMS);
         $paginator->setLimitPerPage(5);
         $paginator->setPage($request->query->getInt('page', 1));
 
-        return $this->render('paginator/index.html.twig', [
-            'paginator' => $paginator,
-        ]);
+        return $paginator;
     }
 }
