34ca3ea6cda80ce6af6bd204b3ed58a0fc02baa3
jmsche <contact@jmsche.fr>
2022-04-04 13:06:43 +0200

    Upgrade vendors & add debug+profiler

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
